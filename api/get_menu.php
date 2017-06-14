f14cbfc012d078f936e009b1e3570c4e1aa86dd4
tukumo <himagami126@gmail.com>
2017-06-14 11:55:43 +0900

    [update]json形式でデータを返すことができるように更新

--- api/get_menu.php
+++ api/get_menu.php
@@ -5,3 +5,20 @@ $result = pg_query($link , 'SELECT * FROM menu where sold_on = ' . $_GET["date"]
 echo json_encode($result);
 pg_close($link);
 ?>
+
+<?php
+$link = pg_connect("host=localhost dbname=team1db user=team1 password=1qazxsw2");
+if (!$link) {
+        print('接続失敗');
+        exit;
+}
+$result = pg_query('SELECT * FROM menus where sold_on = ' . $_GET["date"]);
+if (!$result) {
+        print('クエリ失敗');
+        exit;
+}
+$arrays = pg_fetch_all($result);
+echo json_encode($arrays);
+pg_close($link);
+?>
+
