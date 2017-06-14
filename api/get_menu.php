eff6077587ad542d9a823d2e9443df3a94b5dfeb
tukumo <himagami126@gmail.com>
2017-06-14 12:01:31 +0900

    [fix]間違えた情報を削除

--- api/get_menu.php
+++ api/get_menu.php
@@ -1,12 +1,4 @@
 <?php
-echo $_GET["date"];
-$link = pg_connect("host=localhost dbname=team1db user=team1 password=1qazxsw2");
-$result = pg_query($link , 'SELECT * FROM menu where sold_on = ' . $_GET["date"]);
-echo json_encode($result);
-pg_close($link);
-?>
-
-<?php
 $link = pg_connect("host=localhost dbname=team1db user=team1 password=1qazxsw2");
 if (!$link) {
         print('接続失敗');
