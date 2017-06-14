a97dda933044b1733ab642c5c8def9e58e9023d5
tukumo <himagami126@gmail.com>
2017-06-14 10:43:11 +0900

    [add]get_menuの実装

---
+++ api/get_menu.php
@@ -0,0 +1,6 @@
+<?php
+$link = pg_connect("host=localhost dbname=team1db user=team1 password=1qazxsw2");
+$result = pg_query($link , 'SELECT * FROM menu where sold_on = ' . $_GET["date"]);
+echo json_encode($result);
+pg_close($link);
+?>
