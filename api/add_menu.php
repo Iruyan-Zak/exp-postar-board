a84b939da25236e5f02ab5b0123560264969818f
tukumo <himagami126@gmail.com>
2017-06-21 10:04:45 +0900

    [add]パラメータ確認文の追加

--- api/add_menu.php
+++ api/add_menu.php
@@ -4,6 +4,26 @@ if (!$link) {
     print('接続失敗');
     exit;
 }
+
+if(isset($_GET["name"]){
+    echo $_GET["name"]
+}
+if(isset($_GET["energy"]){
+    echo $_GET["energy"]
+}
+if(isset($_GET["protein"]){
+    echo $_GET["protein"]
+}
+if(isset($_GET["lipid"]){
+    echo $_GET["lipid"]
+}
+if(isset($_GET["salt"]){
+    echo $_GET["salt"]
+}
+if(isset($_GET["sold_on"]){
+    echo $_GET["sold_on"]
+}
+
 $arrays = pg_fetch_all($result);
 echo json_encode($arrays);
 pg_close($link);
