b1ba07304f2fd73d18f3bdf6decb99ad9ddbea22
tukumo <himagami126@gmail.com>
2017-06-21 10:46:41 +0900

    [update]add_menuのクエリを送信できるように更新

--- api/add_menu.php
+++ api/add_menu.php
@@ -4,28 +4,44 @@ if (!$link) {
     print('接続失敗');
     exit;
 }
-
+query = array();
 if(!isset($_GET["name"])){
     echo '不正なパラメータ';
     exit;
 }
-echo $_GET["name"];
+query[] = $_GET['name'];
+
 if(isset($_GET["energy"])){
-    echo $_GET["energy"];
+    query[] = $_GET['energy'];
+} else {
+    query[] = 'null';
 }
 if(isset($_GET["protein"])){
-    echo $_GET["protein"];
+    query[] = $_GET['protein'];
+} else {
+    query[] = 'null';
 }
 if(isset($_GET["lipid"])){
-    echo $_GET["lipid"];
+    query[] = $_GET['lipid'];
+} else {
+    query[] = 'null';
 }
 if(isset($_GET["salt"])){
-    echo $_GET["salt"];
+    query[] = $_GET['salt'];
+} else {
+    query[] = 'null';
 }
 if(isset($_GET["sold_on"])){
-    echo $_GET["sold_on"];
+    query[] = $_GET['sold_on'];
+} else {
+    query[] = 'null';
 }
 
+if(!isset($_GET['id']){
+  $result = pg_query('insert into products (name,energy,protein,lipid,salt) values (' . join(',' , query) . '\'');
+}else{
+    echo '更新処理';
+}
 $arrays = pg_fetch_all($result);
 echo json_encode($arrays);
 pg_close($link);
