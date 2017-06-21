98eab77df4b2747aa2c1f74a2d4c2035c51a4d7f
tukumo <himagami126@gmail.com>
2017-06-21 11:02:51 +0900

    [update]名前だけreturnするように修正

--- api/add_menu.php
+++ api/add_menu.php
@@ -40,7 +40,7 @@ if(isset($_GET["sold_on"])){
 }
 
 if(!isset($_GET['id'])){
-    $result = pg_query('insert into products (name,energy,protein,lipid,salt) values (' . join(',' , query) . ') returning *');
+    $result = pg_query('insert into products (name,energy,protein,lipid,salt) values (' . join(',' , query) . ') returning name');
 }else{
     echo '更新処理';
 }
