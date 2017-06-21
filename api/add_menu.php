13bf5ea9ef86064243c6ea23ee0a43c41329e7cf
tukumo <himagami126@gmail.com>
2017-06-21 10:05:57 +0900

    [fix]セミコロン

--- api/add_menu.php
+++ api/add_menu.php
@@ -6,22 +6,22 @@ if (!$link) {
 }
 
 if(isset($_GET["name"]){
-    echo $_GET["name"]
+    echo $_GET["name"];
 }
 if(isset($_GET["energy"]){
-    echo $_GET["energy"]
+    echo $_GET["energy"];
 }
 if(isset($_GET["protein"]){
-    echo $_GET["protein"]
+    echo $_GET["protein"];
 }
 if(isset($_GET["lipid"]){
-    echo $_GET["lipid"]
+    echo $_GET["lipid"];
 }
 if(isset($_GET["salt"]){
-    echo $_GET["salt"]
+    echo $_GET["salt"];
 }
 if(isset($_GET["sold_on"]){
-    echo $_GET["sold_on"]
+    echo $_GET["sold_on"];
 }
 
 $arrays = pg_fetch_all($result);
