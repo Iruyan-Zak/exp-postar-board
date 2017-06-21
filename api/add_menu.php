8c0c0b173de461cf8432f6be41249cab63d79878
tukumo <himagami126@gmail.com>
2017-06-21 10:03:37 +0900

    [delete]error部分の削除

--- api/add_menu.php
+++ api/add_menu.php
@@ -1,36 +1,9 @@
 <?php
 $link = pg_connect("host=localhost dbname=team1db user=team1 password=1qazxsw2");
 if (!$link) {
-        print('接続失敗');
-        exit;
+    print('接続失敗');
+    exit;
 }
-
-if(isset($_GET["name"]){
-    echo $_GET["name"]
-}
-if(isset($_GET["energy"]){
-    echo $_GET["energy"]
-}
-if(isset($_GET["protein"]){
-    echo $_GET["protein"]
-}
-if(isset($_GET["lipid"]){
-    echo $_GET["lipid"]
-}
-if(isset($_GET["salt"]){
-    echo $_GET["salt"]
-}
-if(isset($_GET["sold_on"]){
-    echo $_GET["sold_on"]
-}
-
-$result = pg_query('SELECT * FROM menus where sold_on = ' . $_GET["date"]);
-if (!$result) {
-        print('クエリ失敗');
-        exit;
-}
-
-
 $arrays = pg_fetch_all($result);
 echo json_encode($arrays);
 pg_close($link);
