38ee6107ea7114c57cf0da869c9dbed250b88625
tukumo <himagami126@gmail.com>
2017-06-28 09:28:11 +0900

    [fix]insertからupdateに修正

--- api/add_menu.php
+++ api/add_menu.php
@@ -50,7 +50,7 @@ if(!isset($_GET['id'])){
     $menu_query[] = pg_query($link,$sql);
 }else{
     $product_query[] = $_GET['id'];
-    $sql = 'insert into products (name,price,energy,protein,lipid,salt,product_id) values (' . join(',' , $product_query) . ') returning product_id';
+    $sql = 'update products set (name,price,energy,protein,lipid,salt,product_id) = (' . join(',' , $product_query) . ') returning product_id';
     $menu_query[] = pg_fetch_row(pg_query($link,$sql))[0];
     $menu_query[] = "'" . $_GET['sold_on'] ."'";
     $sql = 'insert into menus (product_id,sold_on) values (' . join(',' , $menu_query) . ')';
