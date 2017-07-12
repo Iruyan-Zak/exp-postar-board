9dc4680d2159b00fe3777b2ed043875166406364
tukumo <himagami126@gmail.com>
2017-07-12 09:28:06 +0900

    [add]sql処理の追加

--- api/add_star.php
+++ api/add_star.php
@@ -11,11 +11,18 @@ if(!isset($_GET['id'])){
 $sql = 'update menus set star=star+1 where menu_id=' . $_GET['id'] . 'returning star';
 $star = pg_fetch_row(pg_query($link,$sql))[0];
 
-$sql = 'select products.max_star from products,menus where products.product_id=menus.product_id and menus.menu_id=' . $_GET['id']
+$sql = 'select products.max_star from products,menus where products.product_id=menus.product_id and menus.menu_id=' . $_GET['id'];
 $max_star = pg_fetch_row(pg_query($link,$sql))[0];
 
 echo $star;
 echo $max_star;
+
+if($star == $max_star){
+
+    $sql = 'update products.max_star from products,menus where products.product_id=menus.product_id and menus.menu_id=' . $_GET['id'];
+$sql = 'update products set maxstar=' . $star . ') from menus where products.product_id=menus.product_id and menus.menu_id=' . $_GET['id'];
+    pg_query($link,$sql);
+}
 
 pg_close($link);
 ?>
