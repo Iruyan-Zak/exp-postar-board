ec1d1b94d3d1882dbc4bddaf40785f981a81e670
team1 <team1@example.com>
2017-07-19 09:41:43 +0900

    売り切れと星の処理を追加

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ $(function(){
 
             for(var key of keys){
                 let pattern = `#${key}#`;
-                console.log(pattern);
                 dom_str = dom_str.replace(pattern, dict[key] || '-');
             }
 
@@ -26,28 +25,54 @@ $(function(){
                     'menu_id': dict.menu_id
                 });
 
-            console.log(dom);
             table.append(dom);
         }
 
         $('.dropdown')
             .addClass('clickable')
             .click(function() {
-                console.log(this);
                 $(this).parent().next().toggle();
             });
 
-        $('i.fa-times').addClass('clickable').click(function(){
-            // console.log(this);
-            var txt = $(this).closest('tr.menu-row').attr("soldout");
-            if(txt == "false"){
-                $(this).closest('tr.menu-row').attr({soldout:txt.replace(/false/g, "true")});
-            }else if(txt == "true"){
-                $(this).closest('tr.menu-row').attr({soldout:txt.replace(/true/g, "false")});
-            }
-        });
+        $('i.fa-times').addClass('clickable').click(function(){soldOut(this)});
+        $('i.fa-star-o').addClass('clickable').click(function(){fav(this)});
     });
 });
+
+function soldOut(self){
+    console.log(self);
+    var tr = $(self).closest('tr.menu-row');
+    var txt = tr.attr("soldout");
+
+    if(txt === "false"){
+        var new_txt = "true";
+    } else {
+        var new_txt = "false"
+    }
+    tr.attr('soldout', new_txt);
+
+    console.log(tr);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "api/sold_out.php?id=" + tr.attr('menu_id') + "&sold_out=" + new_txt);
+    xhr.send(null);
+
+    return false;
+}
+
+function fav(self){
+    console.log(self);
+    var star = $(self).parent().next();
+    var score = parseInt(star.text()) + 1;
+    star.text(score);
+
+    var tr = $(self).closest('tr.menu-row');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "api/add_star.php?id=" + tr.attr('menu_id'));
+    xhr.send(null);
+
+    return false;
+
+}
 
 function DataSend(self){
     var id = $(self).closest('tr').attr("menu_id");
